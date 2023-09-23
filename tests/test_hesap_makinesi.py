385f3d1246e5e75ff33cedcf43b1ce1ef53d3729
Ercan Kırbıyık <rcnkirbiyik@gmail.com>
2023-09-23 23:15:03 +0300

    new examples and comments added

--- tests/test_hesap_makinesi.py
+++ tests/test_hesap_makinesi.py
@@ -1,3 +1,18 @@
+import pytest
+
+'''
+    'python3 -m pytest -v' komutu ile terminalden bütün testler çalıştırılabilir
+    
+    '@pytest.mark.skip' anatasyonu bu testi atla anlamına gelir ve testi çalıştırmaz, parantez içinde (reason = '') sebebi belirtilebilir
+    
+    'pytest.mark.xfail' anatasyonu testin başarısız olacağını gösteren anatasyondur ve test ignore edilir
+    
+    '@pytest.mark.skipif' anatasyonu parantez içinde verilen koşul sağlanmazsa veya sağlanırsa testi çalıştır anlamına gelir.
+    
+    'python3 -m pytest -vm smoke' komutu ile sadece @pytest.mark.smoke anatasyonu ile işaretlenmiş testler çalıştırılabilir
+'''
+
+sistem = "qa"
 def toplama(a, b):
     return a+b
 
@@ -23,8 +38,27 @@ def test_bölme():
     assert 11.7 != bölme(36, 3.1)
 
 
+# skip anatasyonu bu testi atla anlamına gelir ve testi çalıştırmaz
+@pytest.mark.skip(reason = "Testlerin neden skip edildiğine dair açıklama")
 def test_toplama2():
     assert toplama(4, 7) == 12
 
+
+# xfail anatasyonu bu testin başarısız olacağını gösteren anatasyon ve ignore edecektir
+@pytest.mark.xfail(reason = "Testlerin neden xfail olarak işaretlendiğine dair açıklama")
 def test_çarpma2():
     assert çarpma(12, 1.1) == 13
+
+
+# skipif anatasyonu herhangi bir sebepten bir testin koşmaması ihtimali var ise ve koşumu engelleyen bir durum var ise
+# skip edilmesi için verilen anatasyondur
+@pytest.mark.skipif(sistem == "qa", reason = "qa kodu hatalı olduğu için skip edildi")  # sistem elementi dev yapılır ise test pass olacaktır
+def test_çarpma2():
+    assert 12 == çarpma(3, 4)
+
+
+# smoke anatasyonu testlere karışık olarak verilen spesifik bir anatasyon ve koşulması istenen bütün senaryolara verilerek
+# terminalde 'python3 -m pytest -vm smoke' komutu ile sadece bu testler çalıştırılabilir
+@pytest.mark.smoke
+def test_bölme2():
+    assert 11.7 != bölme(36, 3)
--- tests/test_ücgen_hesaplama.py
+++ tests/test_ücgen_hesaplama.py
@@ -1,12 +1,33 @@
+import pytest
+
+
 def cevre_hesaplama(a, b, c):
     return a+b+c
 
 def alan_hesaplama(taban, yükseklik):
     return  (taban*yükseklik)/2
 
+
+@pytest.mark.smoke
 def test_cevre_hesaplama():
     assert cevre_hesaplama(3, 4, 5) == 12
+    print(" 'Çevre hesaplama' ")
 
+
+# xfail anatasyonu bu testin başarısız olacağını gösteren anatasyon ve ignore edecektir
+@pytest.mark.xfail(reason = "Testlerin neden xfail olarak işaretlendiğine dair açıklama")
+def test_cevre_hesaplama2():
+    assert cevre_hesaplama(3, 4, 7) == 14
+    print(" 'Çevre hesaplama' ")
+
+
+@pytest.mark.smoke
 def test_alan_hesaplama():
+    assert alan_hesaplama(12, 9) == 54
+
+
+# xfail anatasyonu bu testin başarısız olacağını gösteren anatasyon ve ignore edecektir
+@pytest.mark.xfail(reason = "Testlerin neden xfail olarak işaretlendiğine dair açıklama")
+def test_alan_hesaplama2():
     assert alan_hesaplama(12, 9) == 54
 
