8f258b6f8ddc6bbb9a076ad88d9cc79fd45a3e7c
Ercan Kırbıyık <rcnkirbiyik@gmail.com>
2023-09-23 14:17:42 +0300

    pytest examples added

---
+++ tests/test_hesap_makinesi.py
@@ -0,0 +1,30 @@
+def toplama(a, b):
+    return a+b
+
+def çıkarma(a, b):
+    return a-b
+
+def çarpma(a, b):
+    return a*b
+
+def bölme(a, b):
+    return a/b
+
+def test_toplama():
+    assert 4 == toplama(2, 2)
+
+def test_çıkarma():
+    assert çıkarma(3, 2) == 1
+
+def test_çarpma():
+    assert 13 != çarpma(3, 4)
+
+def test_bölme():
+    assert 11.7 != bölme(36, 3.1)
+
+
+def test_toplama2():
+    assert toplama(4, 7) == 12
+
+def test_çarpma2():
+    assert çarpma(12, 0.1) == 1.2
